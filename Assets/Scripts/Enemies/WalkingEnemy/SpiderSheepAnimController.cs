0100a60a6254120d0ff572ef11c7ebfaaa787be1
Exa Byte <jera8068@gmail.com>
2025-07-09 18:54:22 +0500

    Enemy Run Anim
    добавил и настроил анимацию ходьбы врага

---
+++ Assets/Scripts/Enemies/WalkingEnemy/SpiderSheepAnimController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SpiderSheepAnimController : MonoBehaviour
+{
+    //[SerializeField] private Jump jump;
+    //[SerializeField] private Dash dash;
+    private Rigidbody2D rigidbody;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigidbody = GetComponentInParent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //anim.SetFloat("VerticalVelocity", rigidbody.linearVelocityY);
+        //anim.SetBool("IsGrounded", jump.IsGrounded);
+        anim.SetBool("MovmentFalg", rigidbody.linearVelocityX != 0);
+        //anim.SetBool("IsDashing", dash.IsDashing);
+
+    }
+}
